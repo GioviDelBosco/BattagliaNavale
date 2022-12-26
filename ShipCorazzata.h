8eee6c37c412f5c946a6eaa60f477b0a7f58ec7c
GioviDelBosco <giovisgara@gmail.com>
2022-12-26 16:40:06 +0100

    commit generale

---
+++ ShipCorazzata.h
@@ -0,0 +1,21 @@
+#ifndef CORAZZATA
+#define CORAZZATA
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ShipCorazzata{
+    
+    public:
+        ShipCorazzata();
+        void fuoco();
+
+    private:
+        int dimensione;
+        int corazza;
+
+};
+
+#endif
---
+++ ShipSupporto.h
@@ -0,0 +1,20 @@
+#ifndef SUPPORTO
+#define SUPPORTO
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ShipSupporto{
+    public:
+        ShipSupporto();
+        void muoviERipara();
+    private:
+        int dimensione;
+        int corazza;
+
+};
+
+
+#endif
