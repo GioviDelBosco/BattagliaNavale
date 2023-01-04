b3fa238ba1fe64c5da167496a91ba3fdbabf0c39
GioviDelBosco <giovisgara@gmail.com>
2023-01-04 17:15:53 +0100

    commit generale

--- FieldAttacco.cpp
+++ FieldAttacco.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
-void stampaCampo(){
-    
+void stampaCampo(int line, int column, int AttackField[LINE][COL]) {
+    for(int i=1; i<=line; i++){
+        for(int j=1; j<=column; j++){
+            cout<<"|   "<<AttackField[i][j]<<"\t";
+        } 
+        cout<<endl;
+    }
+    cout<<endl;
 }
--- FieldAttacco.h
+++ FieldAttacco.h
@@ -6,12 +6,16 @@
 
 using namespace std;
 
+const int COL = 12;
+const int LINE = 12;
+
 class FieldAttacco{
     
     public:
         FieldAttacco();
-        void StampaCampo();
-
+        void StampaCampo(int line, int column, int AttackField[LINE][COL]);
+    private:
+        char AttackField[LINE][COL];
 
 };
 
